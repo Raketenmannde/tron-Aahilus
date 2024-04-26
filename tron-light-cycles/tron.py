18530ab5ebbf8c13ab349421ad1ddf63d71f36b6
Aahil <irjashaik@gmail.com>
2024-04-26 18:01:07 +0200

    halbwegs 2spieler
    Ich habe fast " Spieler geschafft.

--- tron-light-cycles/tron.py
+++ tron-light-cycles/tron.py
@@ -1,43 +1,68 @@
 # Tron
 import pgzrun
-
+WIDTH=800
+HEIGHT=900
 speed = 3
-
-dirs=[0,90,180,270]
+dirs = [0,90,180,270]
 moves = [(0,-1),(-1,0),(0,1),(1,0)]
 
+score2 = 0
 
-WIDTH = 800
-HEIGHT = 483
-
-def update():
-    global matrix
-    bike.angle = dirs[bike.direction]
-    bike.x += moves[bike.direction][0]*speed
-    bike.y += moves[bike.direction][1]*speed
-
-def on_key_down(key):
-    if key == keys.LEFT:
-        bike.direction += 1
-        if bike.direction == 4 : bike.direction = 0
-
-
-    if key == keys.RIGHT:
-        bike.direction -= 1
-        if bike.direction == -1 : bike.direction = 3
 
 def draw():
     screen.blit("background", (0, 0))
+    for x in range(0, 79):
+        for y in range(0, 59):
+            if matrix[x][y] > 0:
+                matrix[x][y] += 1
+                screen.blit("dot",((x*10)-5,(y*10)-5))
     bike.draw()
-
+    bike2.draw()
+    screen.draw.text("SCORE : "+ str(score), center=(400, 588), owidth=0.5, ocolor=(0,255,255), color=(0,0,255) , fontsize=28)
+def update():
+    global matrix,gamestate,score,score2
+    if gamestate == 0:
+        bike.angle = dirs[bike.direction]
+        bike.x += moves[bike.direction][0]*speed
+        bike.y += moves[bike.direction][1]*speed
+        bike2.angle = dirs[bike.direction]
+        bike2.x += moves[bike.direction][0]*speed
+        bike2.y += moves[bike.direction][1]*speed
+        score += 10
+        score2 += 10
+        if matrix[int(bike.x/10)][int(bike.y/10)] < 15 :
+            matrix[int(bike.x/10)][int(bike.y/10)] += 1
+        else:
+            gamestate = 1
+        if bike.x < 60 or bike.x > 750 or bike.y < 110 or bike.y > 525:
+            gamestate = 1
+    else:
+        if gamestate < 18:
+            bike.image = "bike"+str(int(gamestate/2))
+            bike.angle = dirs[bike.direction]
+            gamestate += 1
+def on_key_down(key):
+    if key == keys.LEFT:
+        bike.direction += 1
+        snapBike()
+        if bike.direction == 4 : bike.direction = 0
+    if key == keys.RIGHT:
+        bike.direction -= 1
+        snapBike()
+        if bike.direction == -1 : bike.direction = 3
+    if key == keys.SPACE and gamestate == 18:
+        init()
+def snapBike():
+    bike.x = int(bike.x/10)*10
+    bike.y = int(bike.y/10)*10
 def init():
-    global bike,matrix
-    bike = Actor('bike1', center=(400, 500))
+    global bike,bike2,matrix,gamestate,score
+    bike = Actor('bike1', center=(300, 500))
     bike.direction = 0
     matrix = [[0 for y in range(60)] for x in range(80)]
-
-
-
-
+    bike2= Actor('bike2', center=(500, 500))
+    bike2.direction = 0
+    matrix = [[0 for y in range(60)] for x in range(80)]
+    gamestate = score = 0
 init()
 pgzrun.go()
